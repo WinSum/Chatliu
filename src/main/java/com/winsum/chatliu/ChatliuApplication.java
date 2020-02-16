b1d28eba336d15804aaaf05a304496b38bd01f5d
Winsum <1007831516@qq.com>
2020-02-16 22:51:21 +0800

    init repo

---
+++ src/main/java/com/winsum/chatliu/ChatliuApplication.java
@@ -0,0 +1,13 @@
+package com.winsum.chatliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatliuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatliuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/winsum/chatliu/ChatliuApplicationTests.java
@@ -0,0 +1,14 @@
+package com.winsum.chatliu;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatliuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
